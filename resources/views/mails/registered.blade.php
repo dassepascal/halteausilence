0b7afc44a97e5b61fc009246479266bc5ac75141
Dasse <pascaldasse56@gmail.com>
2025-08-20 10:53:01 +0200

    add mails/registered

---
+++ resources/views/mails/registered.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ trans('You have been registered') }}</title>
+</head>
+<body>
+    <h1>Bienvenue !</h1>
+    <p>Votre compte a été créé avec succès.</p>
+    <p>Merci de vous être inscrit chez {{ $agence->name }}.</p>
+</body>
+</html>
