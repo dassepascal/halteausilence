d6e1b5065b512e487628723f1963dfd788fc3ef9
Dasse <pascaldasse56@gmail.com>
2025-08-20 17:48:59 +0200

    add contactfactory

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'subject' => $this->faker->sentence(3),
+            'message' => $this->faker->paragraph(3),
+            'is_read' => $this->faker->boolean(30), // 30% de chance d'être lu
+            'created_at' => $this->faker->dateTimeBetween('-6 months', 'now'),
+            'updated_at' => $this->faker->dateTimeBetween('-6 months', 'now'),
+        ];
+    }
+}
